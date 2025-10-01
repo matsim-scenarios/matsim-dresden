f0f33cfcb8287236acb5f400768db5f2ab832f95
sime94 <meinhardt@vsp.tu-berlin.de>
2025-10-01 17:27:31 +0200

    start run class cfg (WIP)

--- src/main/java/org/matsim/run/scenarios/DresdenScenario.java
+++ src/main/java/org/matsim/run/scenarios/DresdenScenario.java
@@ -13,13 +13,25 @@ import org.matsim.application.prepare.network.CleanNetwork;
 import org.matsim.application.prepare.network.CreateNetworkFromSumo;
 import org.matsim.application.prepare.population.*;
 import org.matsim.application.prepare.pt.CreateTransitScheduleFromGtfs;
+import org.matsim.contrib.vsp.pt.fare.DistanceBasedPtFareParams;
+import org.matsim.contrib.vsp.pt.fare.FareZoneBasedPtFareParams;
+import org.matsim.contrib.vsp.pt.fare.PtFareConfigGroup;
+import org.matsim.contrib.vsp.scenario.SnzActivities;
+import org.matsim.contrib.vsp.scoring.RideScoringParamsFromCarParams;
 import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.config.groups.RoutingConfigGroup;
+import org.matsim.core.config.groups.ScoringConfigGroup;
+import org.matsim.core.config.groups.VspExperimentalConfigGroup;
 import org.matsim.core.controler.Controler;
+import org.matsim.core.replanning.annealing.ReplanningAnnealerConfigGroup;
 import org.matsim.prepare.CutOutDresdenPopulation;
 import org.matsim.prepare.PrepareNetwork;
 import org.matsim.prepare.PreparePopulation;
+import org.matsim.simwrapper.SimWrapperConfigGroup;
 import org.matsim.smallScaleCommercialTrafficGeneration.GenerateSmallScaleCommercialTrafficDemand;
 import org.matsim.smallScaleCommercialTrafficGeneration.prepare.CreateDataDistributionOfStructureData;
+import org.matsim.utils.DresdenUtils;
 import picocli.CommandLine;
 
 @CommandLine.Command(header = ":: Dresden Scenario ::", version = DresdenScenario.VERSION, mixinStandardHelpOptions = true)
@@ -41,6 +53,9 @@ public class DresdenScenario extends MATSimApplication {
 	@CommandLine.Mixin
 	private final SampleOptions sample = new SampleOptions(100, 25, 10, 1);
 
+	@CommandLine.Option(names = "--emissions", defaultValue = "ENABLED", description = "Define if emission analysis should be performed or not.")
+	DresdenUtils.FunctionalityHandling emissions;
+
 
 	public DresdenScenario(@Nullable Config config) {
 		super(config);
@@ -58,83 +73,87 @@ public class DresdenScenario extends MATSimApplication {
 	@Override
 	protected Config prepareConfig(Config config) {
 
-//		// Add all activity types with time bins
-//		SnzActivities.addScoringParams(config);
-//
-//		//		add simwrapper config module
-//		SimWrapperConfigGroup simWrapper = ConfigUtils.addOrGetModule(config, SimWrapperConfigGroup.class);
-//
-//		simWrapper.defaultParams().context = "";
-//		simWrapper.defaultParams().mapCenter = "14.5,51.53";
-//		simWrapper.defaultParams().mapZoomLevel = 6.8;
-//		simWrapper.defaultParams().shp = "../shp/oberlausitz.shp";
-//
-//		if (sample.isSet()){
-//			config.controller().setOutputDirectory(sample.adjustName(config.controller().getOutputDirectory()));
-//			config.plans().setInputFile(sample.adjustName(config.plans().getInputFile()));
-//			config.controller().setRunId(sample.adjustName(config.controller().getRunId()));
-//
-//			config.qsim().setFlowCapFactor(sample.getSample());
-//			config.qsim().setStorageCapFactor(sample.getSample());
-//			config.counts().setCountsScaleFactor(sample.getSample());
-//			simWrapper.sampleSize = sample.getSample();
-//		}
-//
-//		config.plans().setActivityDurationInterpretation(PlansConfigGroup.ActivityDurationInterpretation.tryEndTimeThenDuration);
-//
-//		config.vspExperimental().setVspDefaultsCheckingLevel(VspExperimentalConfigGroup.VspDefaultsCheckingLevel.abort);
-//
-//		//		performing set to 6.0 after calibration task force july 24
-//		double performing = 6.0;
-//		ScoringConfigGroup scoringConfigGroup = config.scoring();
-//		scoringConfigGroup.setPerforming_utils_hr(performing);
-//		scoringConfigGroup.setWriteExperiencedPlans(true);
-//		scoringConfigGroup.setPathSizeLogitBeta(0.);
+		// Add all activity types with time bins
+		SnzActivities.addScoringParams(config);
+
+		//		add simwrapper config module
+		SimWrapperConfigGroup simWrapper = ConfigUtils.addOrGetModule(config, SimWrapperConfigGroup.class);
+
+		simWrapper.defaultParams().setContext("");
+		simWrapper.defaultParams().setMapCenter("14.5,51.53");
+		simWrapper.defaultParams().setMapZoomLevel(6.8);
+		simWrapper.defaultParams().setShp("../shp/dresden-utm32n.shp");
+
+		if (sample.isSet()){
+			config.controller().setOutputDirectory(sample.adjustName(config.controller().getOutputDirectory()));
+			config.plans().setInputFile(sample.adjustName(config.plans().getInputFile()));
+			config.controller().setRunId(sample.adjustName(config.controller().getRunId()));
+
+			config.qsim().setFlowCapFactor(sample.getSample());
+			config.qsim().setStorageCapFactor(sample.getSample());
+			config.counts().setCountsScaleFactor(sample.getSample());
+			simWrapper.setSampleSize(sample.getSample());
+		}
+
+		config.vspExperimental().setVspDefaultsCheckingLevel(VspExperimentalConfigGroup.VspDefaultsCheckingLevel.abort);
+
+		//		performing set to 6.0 after calibration task force july 24
+		double performing = 6.0;
+		ScoringConfigGroup scoringConfigGroup = config.scoring();
+		scoringConfigGroup.setPerforming_utils_hr(performing);
+		scoringConfigGroup.setWriteExperiencedPlans(true);
+		scoringConfigGroup.setPathSizeLogitBeta(0.);
+//		TODO: check if the following 2 are needed.
 //		scoringConfigGroup.addActivityParams(new ScoringConfigGroup.ActivityParams("freight_start").setTypicalDuration(30 * 60.));
 //		scoringConfigGroup.addActivityParams(new ScoringConfigGroup.ActivityParams("freight_end").setTypicalDuration(30 * 60.));
-//
-////		set ride scoring params dependent from car params
-////		2.0 + 1.0 = alpha + 1
-////		ride cost = alpha * car cost
-////		ride marg utility of traveling = (alpha + 1) * marg utility travelling car + alpha * beta perf
-//		double alpha = 2;
-//		RideScoringParamsFromCarParams.setRideScoringParamsBasedOnCarParams(scoringConfigGroup, alpha);
-//
-//		config.qsim().setUsingTravelTimeCheckInTeleportation(true);
-//		config.qsim().setUsePersonIdForMissingVehicleId(false);
-//		config.routing().setAccessEgressType(RoutingConfigGroup.AccessEgressType.accessEgressModeToLink);
-//
-////		configure annealing params
-//		config.replanningAnnealer().setActivateAnnealingModule(true);
-//		ReplanningAnnealerConfigGroup.AnnealingVariable annealingVar = new ReplanningAnnealerConfigGroup.AnnealingVariable();
-//		annealingVar.setAnnealType(ReplanningAnnealerConfigGroup.AnnealOption.sigmoid);
-//		annealingVar.setEndValue(0.01);
-//		annealingVar.setHalfLife(0.5);
-//		annealingVar.setShapeFactor(0.01);
-//		annealingVar.setStartValue(0.45);
-//		annealingVar.setDefaultSubpopulation("person");
-//		config.replanningAnnealer().addAnnealingVariable(annealingVar);
-//
-//		//		set pt fare calc model to fareZoneBased = fare of vvo tarifzonen are paid for trips within fare zones
-////		every other trip: Deutschlandtarif
-////		for more info see PTFareModule / ChainedPtFareCalculator classes in vsp contrib
-//		PtFareConfigGroup ptFareConfigGroup = ConfigUtils.addOrGetModule(config, PtFareConfigGroup.class);
-//
-//		FareZoneBasedPtFareParams vvo = new FareZoneBasedPtFareParams();
-//		vvo.setTransactionPartner("VVO Tarifzone 10 Dresden");
-//		vvo.setDescription("VVO Tarifzone 10 Dresden");
-//		vvo.setOrder(1);
-//		vvo.setFareZoneShp("../shp/vvo_tarifzone_10_dresden_utm32n.shp");
-//
-//		DistanceBasedPtFareParams germany = DistanceBasedPtFareParams.GERMAN_WIDE_FARE_2024;
-//		germany.setTransactionPartner("Deutschlandtarif");
-//		germany.setDescription("Deutschlandtarif");
-//		germany.setOrder(2);
-//
-//		ptFareConfigGroup.addParameterSet(vvo);
-//		ptFareConfigGroup.addParameterSet(germany);
-//
-////		TODO: emissions config
+
+//		set ride scoring params dependent from car params
+//		2.0 + 1.0 = alpha + 1
+//		ride cost = alpha * car cost
+//		ride marg utility of traveling = (alpha + 1) * marg utility travelling car + alpha * beta perf
+		double alpha = 2;
+		RideScoringParamsFromCarParams.setRideScoringParamsBasedOnCarParams(scoringConfigGroup, alpha);
+
+		config.qsim().setUsingTravelTimeCheckInTeleportation(true);
+		config.qsim().setUsePersonIdForMissingVehicleId(false);
+		config.routing().setAccessEgressType(RoutingConfigGroup.AccessEgressType.accessEgressModeToLink);
+
+//		configure annealing params
+		config.replanningAnnealer().setActivateAnnealingModule(true);
+		ReplanningAnnealerConfigGroup.AnnealingVariable annealingVar = new ReplanningAnnealerConfigGroup.AnnealingVariable();
+		annealingVar.setAnnealType(ReplanningAnnealerConfigGroup.AnnealOption.sigmoid);
+		annealingVar.setEndValue(0.01);
+		annealingVar.setHalfLife(0.5);
+		annealingVar.setShapeFactor(0.01);
+		annealingVar.setStartValue(0.45);
+		annealingVar.setDefaultSubpopulation("person");
+		config.replanningAnnealer().addAnnealingVariable(annealingVar);
+
+		//		set pt fare calc model to fareZoneBased = fare of vvo tarifzonen are paid for trips within fare zones
+//		every other trip: Deutschlandtarif
+//		for more info see PTFareModule / ChainedPtFareCalculator classes in vsp contrib
+		PtFareConfigGroup ptFareConfigGroup = ConfigUtils.addOrGetModule(config, PtFareConfigGroup.class);
+
+//		TODO: project pt prices to ref year?
+
+		FareZoneBasedPtFareParams vvo = new FareZoneBasedPtFareParams();
+		vvo.setTransactionPartner("VVO Tarifzone 10 Dresden");
+		vvo.setDescription("VVO Tarifzone 10 Dresden");
+		vvo.setOrder(1);
+		vvo.setFareZoneShp("../shp/vvo_tarifzone_10_dresden_utm32n.shp");
+
+		DistanceBasedPtFareParams germany = DistanceBasedPtFareParams.GERMAN_WIDE_FARE_2024;
+		germany.setTransactionPartner("Deutschlandtarif");
+		germany.setDescription("Deutschlandtarif");
+		germany.setOrder(2);
+
+		ptFareConfigGroup.addParameterSet(vvo);
+		ptFareConfigGroup.addParameterSet(germany);
+
+//		TODO: continue run class cfg here
+//		TODO: prepare small scale commercial cfg?
+
+//		TODO: emissions config
 
 		return config;
 	}
