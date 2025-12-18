c94a0aa1d48a09b71c889848d467d1a24d4b2fb3
Kai Nagel <nagel@vsp.tu-berlin.de>
2025-12-18 18:28:17 +0100

    reduce DresdenModelBridge even further

--- src/main/java/org/matsim/run/scenarios/DresdenModelBridge.java
+++ src/main/java/org/matsim/run/scenarios/DresdenModelBridge.java
@@ -1,76 +1,19 @@
 package org.matsim.run.scenarios;
 
-import ch.sbb.matsim.config.SwissRailRaptorConfigGroup;
-import com.google.common.collect.Sets;
-import com.google.inject.multibindings.Multibinder;
 import jakarta.annotation.Nullable;
-import org.matsim.analysis.CheckAndSummarizeLongDistanceFreightPopulation;
-import org.matsim.analysis.CheckStayHomeAgents;
-import org.matsim.analysis.personMoney.PersonMoneyEventsAnalysisModule;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
-import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.application.MATSimApplication;
-import org.matsim.application.analysis.CheckPopulation;
-import org.matsim.application.analysis.traffic.LinkStats;
-import org.matsim.application.options.SampleOptions;
-import org.matsim.application.prepare.CreateLandUseShp;
-import org.matsim.application.prepare.counts.CreateCountsFromBAStData;
-import org.matsim.application.prepare.longDistanceFreightGER.tripExtraction.ExtractRelevantFreightTrips;
-import org.matsim.application.prepare.network.CleanNetwork;
-import org.matsim.application.prepare.network.CreateNetworkFromSumo;
-import org.matsim.application.prepare.population.*;
-import org.matsim.application.prepare.pt.CreateTransitScheduleFromGtfs;
-import org.matsim.contrib.vsp.pt.fare.DistanceBasedPtFareParams;
-import org.matsim.contrib.vsp.pt.fare.FareZoneBasedPtFareParams;
-import org.matsim.contrib.vsp.pt.fare.PtFareConfigGroup;
-import org.matsim.contrib.vsp.pt.fare.PtFareModule;
-import org.matsim.contrib.vsp.scenario.SnzActivities;
-import org.matsim.contrib.vsp.scoring.RideScoringParamsFromCarParams;
 import org.matsim.core.config.Config;
-import org.matsim.core.config.ConfigUtils;
-import org.matsim.core.config.groups.ReplanningConfigGroup;
-import org.matsim.core.config.groups.RoutingConfigGroup;
-import org.matsim.core.config.groups.ScoringConfigGroup;
-import org.matsim.core.config.groups.VspExperimentalConfigGroup;
-import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
-import org.matsim.core.network.NetworkUtils;
-import org.matsim.core.network.turnRestrictions.DisallowedNextLinks;
-import org.matsim.core.replanning.annealing.ReplanningAnnealerConfigGroup;
-import org.matsim.core.replanning.strategies.DefaultPlanStrategiesModule;
-import org.matsim.core.scoring.functions.ScoringParametersForPerson;
-import org.matsim.dashboards.DresdenDashboardProvider;
-import org.matsim.prepare.*;
-import org.matsim.simwrapper.DashboardProvider;
-import org.matsim.simwrapper.SimWrapperConfigGroup;
-import org.matsim.simwrapper.SimWrapperModule;
-import org.matsim.smallScaleCommercialTrafficGeneration.GenerateSmallScaleCommercialTrafficDemand;
-import org.matsim.smallScaleCommercialTrafficGeneration.prepare.CreateDataDistributionOfStructureData;
-import picocli.CommandLine;
-import playground.vsp.scoring.IncomeDependentUtilityOfMoneyPersonScoringParameters;
 
-import java.util.HashSet;
 import java.util.Set;
 
-import static org.matsim.utils.DresdenUtils.*;
+public final class DresdenModelBridge extends DresdenModel {
+	// "final": for the time being, please try to avoid inheritance from inheritance.  kai, dec'25
 
-@CommandLine.Command(header = ":: Dresden Scenario ::", version = DresdenModelBridge.VERSION, mixinStandardHelpOptions = true)
-@MATSimApplication.Prepare({
-		CreateNetworkFromSumo.class, CreateTransitScheduleFromGtfs.class, TrajectoryToPlans.class, GenerateShortDistanceTrips.class,
-		MergePopulations.class, ExtractRelevantFreightTrips.class, DownSamplePopulation.class, ExtractHomeCoordinates.class,
-		CreateLandUseShp.class, ResolveGridCoordinates.class, FixSubtourModes.class, AdjustActivityToLinkDistances.class, XYToLinks.class,
-		CleanNetwork.class, PrepareNetwork.class, SplitActivityTypesDuration.class, CreateCountsFromBAStData.class,
-		CutOutDresdenPopulation.class, CreateDataDistributionOfStructureData.class, GenerateSmallScaleCommercialTrafficDemand.class,
-		PreparePopulation.class, CreateFacilitiesFromPopulation.class, CreateSingleTransportModePopulation.class, RemoveVehicleInformationFromPopulation.class
-})
-@MATSimApplication.Analysis({
-		LinkStats.class, CheckPopulation.class, CheckAndSummarizeLongDistanceFreightPopulation.class, CheckStayHomeAgents.class
-})
-public class DresdenModelBridge extends DresdenModel {
-
-	public static final String VERSION = "v1.0";
+//	public static final String VERSION = "v1.0";
 
 	public static void main(String[] args) {
 		if ( args != null && args.length > 0 ) {
