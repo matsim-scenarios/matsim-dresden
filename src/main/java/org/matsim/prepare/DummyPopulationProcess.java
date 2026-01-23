8a801610a0aaee7e7119b1c7a3ec3feca10424e0
Daniel Roeder  (VSP) <d-roeder@users.noreply.github.com>
2026-01-23 17:51:24 +0100

    fix checkstyle

--- src/main/java/org/matsim/prepare/DummyPopulationProcess.java
+++ src/main/java/org/matsim/prepare/DummyPopulationProcess.java
@@ -16,33 +16,29 @@ import picocli.CommandLine;
 		name = "DummyPopulationProcess",
 		description = "read a configuration, read the population, maipulate the config and write an adapted config that pointers to the new population"
 )
-public class DummyPopulationProcess  implements MATSimAppCommand {
+public class DummyPopulationProcess implements MATSimAppCommand {
 
 
 	@CommandLine.Option(names = "--inputconfig", description = "path to the input-config", required = true)
 	private String inputconfig;
-	
+
 	@CommandLine.Option(names = "--outputpath", description = "outputpath", required = true)
 	private String outputpath;
-	
+
 	private static final String CONFIG = "config.xml";
-	private static String POPULATIONFILE = "population.xml.gz";
+	private static final String POPULATIONFILE = "population.xml.gz";
 
 	@Override
 	public Integer call() throws Exception {
-		try {
-			Config config = ConfigUtils.loadConfig(inputconfig);
-			Scenario scenario = ScenarioUtils.loadScenario(config);
-			
-			String outputplans =  new File(outputpath, POPULATIONFILE).getAbsolutePath();
-			new PopulationWriter(scenario.getPopulation()).write(outputplans);
-			
-			config.plans().setInputFile(outputplans);
-			String outputconfig = new File(outputpath, CONFIG).getAbsolutePath();
-			new ConfigWriter(config).write(outputconfig);
-		} catch (Exception e) {
-			return 1;
-		}
+		Config config = ConfigUtils.loadConfig(inputconfig);
+		Scenario scenario = ScenarioUtils.loadScenario(config);
+
+		String outputplans = new File(outputpath, POPULATIONFILE).getAbsolutePath();
+		new PopulationWriter(scenario.getPopulation()).write(outputplans);
+
+		config.plans().setInputFile(outputplans);
+		String outputconfig = new File(outputpath, CONFIG).getAbsolutePath();
+		new ConfigWriter(config).write(outputconfig);
 		return 0;
 	}
 
