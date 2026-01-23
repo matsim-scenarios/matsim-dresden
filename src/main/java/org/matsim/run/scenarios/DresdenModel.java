cad309a6c38adf68b4b605bf678afd263156117a
Daniel Roeder  (VSP) <d-roeder@users.noreply.github.com>
2026-01-23 15:36:21 +0100

    add a dummy-process that should not be pushed into main/production

--- src/main/java/org/matsim/run/scenarios/DresdenModel.java
+++ src/main/java/org/matsim/run/scenarios/DresdenModel.java
@@ -64,7 +64,8 @@ import static org.matsim.utils.DresdenUtils.*;
 		CreateLandUseShp.class, ResolveGridCoordinates.class, FixSubtourModes.class, AdjustActivityToLinkDistances.class, XYToLinks.class,
 		CleanNetwork.class, PrepareNetwork.class, SplitActivityTypesDuration.class, CreateCountsFromBAStData.class,
 		CutOutDresdenPopulation.class, CreateDataDistributionOfStructureData.class, GenerateSmallScaleCommercialTrafficDemand.class,
-		PreparePopulation.class, CreateFacilitiesFromPopulation.class, CreateSingleTransportModePopulation.class, RemoveVehicleInformationFromPopulation.class
+		PreparePopulation.class, CreateFacilitiesFromPopulation.class, CreateSingleTransportModePopulation.class, RemoveVehicleInformationFromPopulation.class,
+		DummyPopulationProcess.class
 })
 @MATSimApplication.Analysis({
 		LinkStats.class, CheckPopulation.class, CheckAndSummarizeLongDistanceFreightPopulation.class, CheckStayHomeAgents.class
