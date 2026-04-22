d950d4e3ee38d8f0c63130a12d25bb371b0dee4f
Paul Heinrich <55242299+paulheinr@users.noreply.github.com>
2026-04-22 15:04:03 +0200

    remove default 10% config

--- src/main/java/org/matsim/run/scenarios/DresdenScenario.java
+++ src/main/java/org/matsim/run/scenarios/DresdenScenario.java
@@ -85,7 +85,7 @@ public class DresdenScenario extends MATSimApplication {
 	}
 
 	public DresdenScenario() {
-		super(String.format("input/%s/dresden-%s-10pct.config.xml", VERSION, VERSION));
+		super();
 	}
 
 	public static void main(String[] args) {
